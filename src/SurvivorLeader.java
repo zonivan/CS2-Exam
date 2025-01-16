f756807aa00f31e827bd053a075cf2255cfe9355
ebradford <emma_bradford@milton.edu>
2025-01-16 10:49:15 -0500

    Initial commit

---
+++ src/SurvivorLeader.java
@@ -0,0 +1,3 @@
+public class SurvivorLeader {
+    private String transmission = "q2x1t8r5w3m9c5i8l0d5n7o3g5t5y6e7b5r5l9a5k2k5e7q5t5p4o5i3n6z5o8l5d5k7j5o4k5k5m5o6k5k9c5i8t5y3f2v1q9x7z6t5m8r9j3o4v5p3l1w9y4d6f7x2c9l5h8s4k6";
+}
