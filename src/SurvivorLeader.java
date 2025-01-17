238d8a4266c44e7aa61ce003f01cc50fe49b3880
ebradford <emma_bradford@milton.edu>
2025-01-17 12:08:20 -0500

    transmission

--- src/SurvivorLeader.java
+++ src/SurvivorLeader.java
@@ -1,3 +1,3 @@
 public class SurvivorLeader {
-    private String transmission = "q2x1t8r5w3m9c5i8l0d5n7o3g5t5y6e7b5r5l9a5k2k5e7q5t5p4o5i3n6z5o8l5d5k7j5o4k5k5m5o6k5k9c5i8t5y3f2v1q9x7z6t5m8r9j3o4v5p3l1w9y4d6f7x2c9l5h8s4k6";
+    private String transmission = "q2xf1t8r5w3t tm9c5i8l900d5n7heto 3g5t6hgw44y5e7bhty605rh8t5 85ml95ak2k5r7qw5ksp295e5t7p45 o5i3n 6z5n205 9soe5o8hhg5lygtt5dq7th 5 gf34s9o 35j7j5o45k99hg5kyqqcjg5mkpl20z5o629sle5k20sk5k9c5 gh73h qxp25vi8t5i3f2v1q 9x7z6t5l8r9j3o4v5l3l1w 9y5a4d6f7x2c9l5g h8s45ek6";
 }
